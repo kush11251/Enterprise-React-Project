12b7240d985a3342a9115d857d115f4b0e7b1dbe
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-18 12:01:43 +0530

    feat: integrate src/config/config.ts

---
+++ src/config/config.ts
@@ -0,0 +1,10 @@
+// src/config/config.ts
+export const config = {
+  database: {
+    user: 'postgres',
+    host: 'localhost',
+    database: 'mydb',
+    password: 'password',
+    port: 5432,
+  },
+};
