5bcc855e2dbfb2d191a30f852e01b674df9cc7d0
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-18 12:01:30 +0530

    feat: integrate src/components/App.tsx

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+// src/components/App.tsx
+import React from 'react';
+import PrimaryButton from './ui/buttons/PrimaryButton';
+const App = () => {
+  return <div><PrimaryButton>Click me!</PrimaryButton></div>;
+};
+export default App;
