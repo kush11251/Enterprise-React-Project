04f02d670d2e04a89256d2b6757c18e44dfd42a6
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-18 12:01:32 +0530

    feat: integrate src/components/ui/buttons/PrimaryButton.tsx

---
+++ src/components/ui/buttons/PrimaryButton.tsx
@@ -0,0 +1,6 @@
+// src/components/ui/buttons/PrimaryButton.tsx
+import React from 'react';
+const PrimaryButton = (props: any) => {
+  return <button style={{ backgroundColor: 'blue', color: 'white' }} {...props} />;
+};
+export default PrimaryButton;
